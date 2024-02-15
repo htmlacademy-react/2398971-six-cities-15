7ac41396616db8cd3abfea14e2092c8c93a3229a
MrScience969 <scieence969@gmail.com>
2024-02-15 21:12:33 +0700

    Начинаем программировать

---
+++ src/components/app/app.tsx
@@ -0,0 +1,14 @@
+import MainScreen from '../../pages/main-screen/main-screen';
+
+type AppScreenProps = {
+  placeCardCount: number;
+}
+
+function App ({placeCardCount}: AppScreenProps): JSX.Element {
+
+  return (
+    <MainScreen placeCardCount = {placeCardCount}/>
+  );
+}
+
+export default App;

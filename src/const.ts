f21a63b16318879b4592478ebbcde0a945d57d8b
MrScience969 <scieence969@gmail.com>
2024-03-31 15:43:05 +0700

    Вернут dusseldorf

--- src/const.ts
+++ src/const.ts
@@ -60,7 +60,7 @@ export const CITIES = [
   },
   {
     'id': 'dusseldorf',
-    'name': 'London',
+    'name': 'Dusseldorf',
     'location': {'latitude': 51.225402, 'longitude': 6.776314, 'zoom': 13}
   },
 ];

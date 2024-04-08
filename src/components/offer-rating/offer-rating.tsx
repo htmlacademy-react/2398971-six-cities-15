e3f3eda897c81d3028bad4d2e6a5c44ec2f7131a
MrScience969 <scieence969@gmail.com>
2024-04-08 21:18:16 +0700

    Добавит округление рейтинга для отрисовки звезд

--- src/components/offer-rating/offer-rating.tsx
+++ src/components/offer-rating/offer-rating.tsx
@@ -9,7 +9,7 @@ function OfferRating(props:OfferRatingProps): JSX.Element {
   return (
     <div className={`${className}__rating rating`} data-testid="offer-rating">
       <div className={`${className}__stars rating__stars`}>
-        <span style={{ width: `${rating / 5 * 100}%`}} />
+        <span style={{ width: `${Math.round(rating) / 5 * 100}%`}} />
         <span className="visually-hidden">Rating</span>
       </div>
       {className === 'offer' ? <span className={`${className}__rating-value rating__value`}>{rating}</span> : ''}
